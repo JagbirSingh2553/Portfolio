6ba41476937c26ce41a2847d2423c0357bdd5854
Jagbir Singh <jg581261@dal.ca>
2025-04-15 15:20:02 +0530

    Update in Portfolio

--- src/Components/Experience/experience.jsx
+++ src/Components/Experience/experience.jsx
@@ -7,9 +7,17 @@ const Experience = () => {
       <h2>Experience</h2>
       <ul>
         <li>Infosys: System Engineer (March 2021 - Aug 2022)</li>
-        <li>At Infosys, I managed Azure cloud policy and technology requests within a five-member team utilizing Agile methodologies.</li>
-          <li>I led an Agile project team, which earned an 'Appreciation Award' for excellence in project management and communication. </li>
-          <li>Additionally, I implemented reporting tools to streamline resolution processes, improving user satisfaction and optimizing workflow efficiency.</li>
+        <li>• At Infosys, I managed Azure cloud policy and technology requests within a five-member team utilizing Agile methodologies.</li>
+        <li>• I led an Agile project team, which earned an 'Appreciation Award' for excellence in project management and communication. </li>
+        <li>• Additionally, I implemented reporting tools to streamline resolution processes, improving user satisfaction and optimizing workflow efficiency.</li>
+      </ul>
+      <ul>
+        <li>Integrated Technologies: Data Analyst (Oct 2019 - Mar 2021)</li>
+        <li>•	Conducted data analysis, data integration, and data governance initiatives to streamline workflows.</li>
+        <li>•	Built conceptual and logical data models, improving data architecture scalability.</li>
+        <li>•	Developed data visualizations and dashboards in Power BI and Tableau, driving strategic decision-making for stakeholders.</li>
+        <li>•	Worked on data cleansing, preprocessing, and transformation to improve the quality and integrity of business-critical data.</li>
+        <li>•	Supported data governance and compliance efforts, ensuring adherence to industry best practices and security protocols.</li>
       </ul>
     </section>
   );
